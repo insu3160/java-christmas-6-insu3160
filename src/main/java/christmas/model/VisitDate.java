febe86b3dbc121e80a39f423910b9acb32937297
insu <is23193160@gmail.com>
2023-11-11 20:20:22 +0900

    feat: 1이상 31 이하의 방문 날짜가 아닐 경우 예외 처리하는 기능 추가

--- src/main/java/christmas/model/VisitDate.java
+++ src/main/java/christmas/model/VisitDate.java
@@ -1,12 +1,26 @@
 package christmas.model;
 
+import christmas.contants.ErrorMessages;
 import java.time.LocalDate;
 
 public class VisitDate {
+    private final static int START_DAY = 1;
+    private final static int END_DAY = 31;
     private final LocalDate visitDate;
 
     public VisitDate(int visitDay) {
+        validate(visitDay);
         this.visitDate = LocalDate.of(2023, 12, visitDay);
+    }
+
+    private void validate(int visitDay) {
+        ensureValidDay(visitDay);
+    }
+
+    private void ensureValidDay(int visitDay) {
+        if (visitDay < START_DAY || visitDay > END_DAY) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_DATE_ERROR_MESSAGE);
+        }
     }
 
 }
