a089d2e3a6eaceb32616c9426b41a790552e4623
insu <is23193160@gmail.com>
2023-11-11 21:15:30 +0900

    test: 메뉴가 존재하지 않을 경우 예외 처리하는지 테스트 작성

--- src/test/java/christmas/enums/MenuTest.java
+++ src/test/java/christmas/enums/MenuTest.java
@@ -1,7 +1,9 @@
 package christmas.enums;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import christmas.contants.ErrorMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,13 @@ class MenuTest {
     void returnMenuIfExistsOnMenuBoard() {
         assertThat(Menu.findMenu("양송이수프")).isEqualTo(Menu.YANGSONG_SOUP);
     }
-    
+
+    @Test
+    @DisplayName("메뉴판에 없는 메뉴를 입력하였을 경우 예외 처리한다.")
+    void inputUnrecognizedMenu() {
+        assertThatThrownBy(() -> Menu.findMenu("영화"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_ORDER_ERROR_MESSAGE);
+    }
+
 }
