82af122445bb5ea72ff5fcf4e3afa4a1ed672244
insu <is23193160@gmail.com>
2023-11-11 18:50:43 +0900

    feat: 플래너 시작 문구 출력하는 기능 추가

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+    private final static String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public static void welcomeEventPlanner() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+
+}
