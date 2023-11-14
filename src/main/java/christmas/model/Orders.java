12ab376323c3d3fa864bf4f91afee12ec608e72a
insu <is23193160@gmail.com>
2023-11-14 10:59:24 +0900

    feat: 중복 메뉴를 입력한 경우 예외 처리하는 기능 추가

--- src/main/java/christmas/model/Orders.java
+++ src/main/java/christmas/model/Orders.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import christmas.contants.ErrorMessages;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,16 @@ public class Orders {
     private List<Order> orders = new ArrayList<>();
 
     public void record(Order order) {
+        validate(order);
         orders.add(order);
+    }
+    private void validate(Order order) {
+        checkForDuplicates(order);
+    }
+    private void checkForDuplicates(Order order) {
+        if (orders.contains(order)) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_ORDER_ERROR_MESSAGE);
+        }
     }
 
 }
