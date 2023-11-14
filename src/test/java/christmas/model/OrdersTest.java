c30e65c8e05407339cf1772bf6b70601bd450842
insu <is23193160@gmail.com>
2023-11-14 11:13:53 +0900

    test: 중복된 메뉴가 추가 될 시 예외 처리하는지 확인하는 테스트 추가

---
+++ src/test/java/christmas/model/OrdersTest.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.contants.ErrorMessages;
+import christmas.enums.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrdersTest {
+    @Test
+    @DisplayName("중복된 메뉴가 추가 될 시 예외 처리한다.")
+    void testRecordDuplicateOrder(){
+        Orders orders = new Orders();
+        Order firstOrder = new Order(Menu.BBQ_RIBS,new MenuQuantity(2));
+        Order secondOrder = new Order(Menu.BBQ_RIBS,new MenuQuantity(1));
+        orders.record(firstOrder);
+
+        assertThatThrownBy(() -> orders.record(secondOrder))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_ORDER_ERROR_MESSAGE);
+    }
+
+}
