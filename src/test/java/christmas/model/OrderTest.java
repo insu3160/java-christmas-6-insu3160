799202c68628c456a7c7d78e61a1d46c14c4faea
insu <is23193160@gmail.com>
2023-11-14 15:16:06 +0900

    test: 넘겨받은 카테고리가 일치할 때 true를 반환하는지 확인하는 테스트 추가

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -2,6 +2,7 @@ package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.enums.Category;
 import christmas.enums.Menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,15 @@ class OrderTest {
         Order secondOrder = new Order(menu, secondQuantity);
 
         assertThat(firstOrder.equals(secondOrder)).isTrue();
+    }
+    @Test
+    @DisplayName("넘겨 받은 카테고리가 일치할 때 true를 반환한다.")
+    void testMatchedCategory(){
+        Menu menu = Menu.CHAMPAGNE;
+        MenuQuantity firstQuantity = new MenuQuantity(3);
+        Order firstOrder = new Order(menu, firstQuantity);
+
+        assertThat(firstOrder.matchedCategory(Category.BEVERAGE)).isTrue();
     }
 
 }
