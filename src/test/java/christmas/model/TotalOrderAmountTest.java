d06cad073ad790d50ea963f6ef9faa7b9dd1c34d
insu <is23193160@gmail.com>
2023-11-14 23:27:22 +0900

    test: 증정 이벤트에 참여할 수 있다면 true를 반환하는지 확인하는 테스트 추가

--- src/test/java/christmas/model/TotalOrderAmountTest.java
+++ src/test/java/christmas/model/TotalOrderAmountTest.java
@@ -1,7 +1,6 @@
 package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.service.dto.TotalOrderAmountDto;
 import org.junit.jupiter.api.DisplayName;
@@ -10,20 +9,29 @@ import org.junit.jupiter.api.Test;
 class TotalOrderAmountTest {
     @Test
     @DisplayName("TotalOrderAmountDto로 잘 반환하는지 확인한다.")
-    void testConvertToTotalOrderAmountDto(){
+    void testConvertToTotalOrderAmountDto() {
         TotalOrderAmount totalOrderAmount = new TotalOrderAmount(15000);
         TotalOrderAmountDto totalOrderAmountDto = totalOrderAmount.convertToTotalOrderAmountDto();
 
         assertThat(totalOrderAmountDto.totalOrderAmount()).isEqualTo(15000);
     }
+
     @Test
     @DisplayName("이벤트에 참여할 수 있는 금액이면 true를 반환한다.")
-    void testCanParticipateInEvent(){
+    void testCanParticipateInEvent() {
         TotalOrderAmount cantParticipateEvent = new TotalOrderAmount(8000);
         TotalOrderAmount canParticipateEvent = new TotalOrderAmount(12000);
 
         assertThat(cantParticipateEvent.canParticipateInEvent()).isFalse();
         assertThat(canParticipateEvent.canParticipateInEvent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("증정 이벤트에 참여할 수 있다면 true를 반환한다.")
+    void testCanGetGift() {
+        TotalOrderAmount totalOrderAmount = new TotalOrderAmount(130000);
+
+        assertThat(totalOrderAmount.canGetGiftMenu()).isTrue();
     }
 
 }
