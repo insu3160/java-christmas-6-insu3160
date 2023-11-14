f02962c38d625d4383d30d1826405e3ca971df84
insu <is23193160@gmail.com>
2023-11-14 10:34:39 +0900

    test: 메뉴 갯수가 1개 미만일 경우 예외 처리하는지 확인하는 테스트 추가

--- src/test/java/christmas/model/MenuQuantityTest.java
+++ src/test/java/christmas/model/MenuQuantityTest.java
@@ -1,7 +1,9 @@
 package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import christmas.contants.ErrorMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,14 @@ class MenuQuantityTest {
         MenuQuantity menuQuantity = new MenuQuantity(3);
 
         assertThat(menuQuantity.getMenuQuantity()).isEqualTo(validQuantity);
+    }
+
+    @Test
+    @DisplayName("메뉴 갯수가 1개 미만일 경우 예외가 발생한다.")
+    void testInvalidMenuQuantity() {
+        assertThatThrownBy(() -> new MenuQuantity(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessages.INVALID_ORDER_ERROR_MESSAGE);
     }
 
 }
