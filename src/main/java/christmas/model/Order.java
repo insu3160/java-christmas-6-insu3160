bd10aa17e7ca7a63231763a9a17d1ab3b32881b0
insu <is23193160@gmail.com>
2023-11-14 10:54:10 +0900

    feat: 메뉴가 같을 시 같은 객체로 인식하는 기능 추가

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.enums.Menu;
+import java.util.Objects;
 
 public class Order {
     private final Menu menu;
@@ -9,6 +10,23 @@ public class Order {
     public Order(Menu menu, MenuQuantity menuQuantity) {
         this.menu = menu;
         this.menuQuantity = menuQuantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(menu, order.menu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menu);
     }
 
 }
