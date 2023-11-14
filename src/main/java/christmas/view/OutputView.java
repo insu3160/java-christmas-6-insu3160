940d45ccf965ab22046847f5a8faee5d71da77b3
insu <is23193160@gmail.com>
2023-11-14 14:54:28 +0900

    refactor: 출력문 상수로 처리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,6 +8,7 @@ public class OutputView {
     private final static String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private final static String EVENT_BENEFITS_DATE_MESSAGE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     private final static String ORDER_MENU_TITLE = "<주문 메뉴>";
+    private static final String ORDER_FORMAT = "%s %d개";
 
     public static void welcomeEventPlanner() {
         System.out.println(WELCOME_MESSAGE);
@@ -24,7 +25,7 @@ public class OutputView {
     public static void displayOrders(List<OrderDto> orderDtos) {
         System.out.println(ORDER_MENU_TITLE);
         for (OrderDto orderDto : orderDtos) {
-            System.out.println(orderDto.menu().getMenuName() + " " + orderDto.menuQuantity() + "개");
+            System.out.println(String.format(ORDER_FORMAT,orderDto.menu().getMenuName(),orderDto.menuQuantity()));
         }
     }
 
