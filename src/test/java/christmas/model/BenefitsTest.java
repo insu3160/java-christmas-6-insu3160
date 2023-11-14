3be50737286c82708b29148e0cfd8223332298a8
insu <is23193160@gmail.com>
2023-11-15 01:02:51 +0900

    refactor: 테스트 코드에 사용된 변수명 giftBenefit -> weekDayBenefit으로 변경

--- src/test/java/christmas/model/BenefitsTest.java
+++ src/test/java/christmas/model/BenefitsTest.java
@@ -1,7 +1,6 @@
 package christmas.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import christmas.enums.Event;
 import christmas.enums.GiftMenu;
@@ -11,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class BenefitsTest {
     @Test
     @DisplayName("증정 이벤트가 존재할 때 증정 메뉴를 잘 반환하는지 확인한다.")
-    void testGetGiftMenuWithMatchingEvent(){
+    void testGetGiftMenuWithMatchingEvent() {
         Benefits benefits = new Benefits();
         Benefit giftBenefit = new Benefit(Event.GIFT, -2023);
         benefits.add(giftBenefit);
@@ -20,12 +19,13 @@ class BenefitsTest {
 
         assertThat(result).isEqualTo(GiftMenu.GIFT_MENU);
     }
+
     @Test
     @DisplayName("증정 이벤트가 존재하지 않을 때 증정 메뉴를 잘 반환하는지 확인한다.")
-    void testGetGiftMenuWithOutMatchingEvent(){
+    void testGetGiftMenuWithOutMatchingEvent() {
         Benefits benefits = new Benefits();
-        Benefit giftBenefit = new Benefit(Event.WEEKDAY, -2023);
-        benefits.add(giftBenefit);
+        Benefit weekDayBenefit = new Benefit(Event.WEEKDAY, -2023);
+        benefits.add(weekDayBenefit);
 
         GiftMenu result = benefits.getGiftMenu();
 
