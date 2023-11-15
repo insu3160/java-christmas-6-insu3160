897906f1ce0b6b28e9b04fee1ba9524bb6d297dd
insu <is23193160@gmail.com>
2023-11-15 10:30:31 +0900

    refactor: Menu.CHAMPAGNE이 여러번 사용되므로 상수로 처리

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
+    private static final Menu TEST_MENU = Menu.CHAMPAGNE;
+
     @Test
     @DisplayName("메뉴가 같을 시 같은 객체로 인식하는지 확인한다.")
     void testEqualsOrder() {
-        Menu menu = Menu.CHAMPAGNE;
         MenuQuantity firstQuantity = new MenuQuantity(3);
-        MenuQuantity secondQuantity = new MenuQuantity(2);
-        Order firstOrder = new Order(menu, firstQuantity);
-        Order secondOrder = new Order(menu, secondQuantity);
+        Order firstOrder = new Order(TEST_MENU, firstQuantity);
+        Order secondOrder = new Order(TEST_MENU, new MenuQuantity(2));
 
         assertThat(firstOrder.equals(secondOrder)).isTrue();
     }
@@ -24,9 +24,8 @@ class OrderTest {
     @Test
     @DisplayName("넘겨 받은 카테고리가 일치할 때 true를 반환한다.")
     void testMatchedCategory() {
-        Menu menu = Menu.CHAMPAGNE;
         MenuQuantity firstQuantity = new MenuQuantity(3);
-        Order firstOrder = new Order(menu, firstQuantity);
+        Order firstOrder = new Order(TEST_MENU, firstQuantity);
 
         assertThat(firstOrder.matchedCategory(Category.BEVERAGE)).isTrue();
     }
@@ -34,24 +33,22 @@ class OrderTest {
     @Test
     @DisplayName("OrderDto를 잘 반환하는지 확인한다.")
     void testConvertToOrderDto() {
-        Menu menu = Menu.CHAMPAGNE;
         MenuQuantity firstQuantity = new MenuQuantity(3);
-        Order firstOrder = new Order(menu, firstQuantity);
+        Order firstOrder = new Order(TEST_MENU, firstQuantity);
 
         OrderDto firstOrderDto = firstOrder.convertToOrderDto();
 
-        assertThat(firstOrderDto.menu()).isEqualTo(menu);
+        assertThat(firstOrderDto.menu()).isEqualTo(TEST_MENU);
         assertThat(firstOrderDto.menuQuantity()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("메뉴마다의 총가격을 잘 반환하는지 확인한다.")
     void testCalculatePrice() {
-        Menu menu = Menu.CHAMPAGNE;
         MenuQuantity firstQuantity = new MenuQuantity(3);
-        Order firstOrder = new Order(menu, firstQuantity);
+        Order firstOrder = new Order(TEST_MENU, firstQuantity);
 
-        assertThat(firstOrder.caculatePrice()).isEqualTo(Menu.CHAMPAGNE.getPrice() * firstQuantity.getMenuQuantity());
+        assertThat(firstOrder.caculatePrice()).isEqualTo(TEST_MENU.getPrice() * firstQuantity.getMenuQuantity());
     }
 
 }
